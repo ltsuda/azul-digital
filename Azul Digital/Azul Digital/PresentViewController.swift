2b5c18186c111b2487b156ce5dcd85e9912a2e04
ltsuda <leonardo.tsuda@gmail.com>
2016-07-11 15:25:00 -0300

    add pods, create presentApp screen layout

---
+++ Azul Digital/Azul Digital/PresentViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PresentViewController.swift
+//  Azul Digital
+//
+//  Created by Leonardo Tsuda on 7/11/16.
+//  Copyright © 2016 Leonardo Tsuda. All rights reserved.
+//
+
+import UIKit
+
+class PresentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Azul Digital/Pods/Target Support Files/Pods-Azul Digital/Pods-Azul Digital-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Azul_DigitalVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Azul_DigitalVersionString[];
+
