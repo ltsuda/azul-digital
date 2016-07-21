07e1c562e476a04b10cc2019ab0564e8c9f475d1
ltsuda <leonardo.tsuda@gmail.com>
2016-07-21 18:11:01 -0300

    add UIImagePickerController to choose an image to user's image profile

--- Azul Digital/Azul Digital/Register/ProfileViewController.swift
+++ Azul Digital/Azul Digital/Register/ProfileViewController.swift
@@ -43,8 +43,7 @@ class ProfileViewController: UIViewController, alertable, profile {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-
-        
+        profileImageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleSelectProfileImageView)))
         
     }
     
@@ -78,6 +77,34 @@ class ProfileViewController: UIViewController, alertable, profile {
         }
         
     }
- 
 
+}
+
+extension ProfileViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func handleSelectProfileImageView() {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        
+        var selectedImageFromPicker: UIImage?
+        
+        if let editedImage = info["UIImagePickerControllerEditedImage"] as? UIImage {
+            selectedImageFromPicker = editedImage
+        } else if let originalImage = info["UIImagePickerControllerOriginalImage"] as? UIImage {
+            selectedImageFromPicker = originalImage
+        }
+        if let selectedImage = selectedImageFromPicker {
+            DispatchQueue.main.async {
+                self.profileImageView.image = selectedImage
+            }
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
 }
