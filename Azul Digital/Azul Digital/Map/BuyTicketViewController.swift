99033f32e0dd55178152cc7012186f8c591cea15
ltsuda <leonardo.tsuda@gmail.com>
2016-09-05 23:19:19 -0300

    change how statusBarStyle is set, start implement BuyTicket

---
+++ Azul Digital/Azul Digital/Map/BuyTicketViewController.swift
@@ -0,0 +1,35 @@
+//
+//  BuyTicketViewController.swift
+//  Azul Digital
+//
+//  Created by Leonardo Tsuda on 9/5/16.
+//  Copyright © 2016 Leonardo Tsuda. All rights reserved.
+//
+
+import UIKit
+
+class BuyTicketViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
